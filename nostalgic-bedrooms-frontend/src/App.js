c483a151b6f8fe10c668b7f5f0bb69037cd32b6d
Tiffany <tbcasper16@gmail.com>
2024-08-07 14:19:15 -0500

    adding custom scrollbar

--- nostalgic-bedrooms-frontend/src/App.js
+++ nostalgic-bedrooms-frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BedroomList from './components/BedroomList';
 import './App.css';
 import { MouseTrail } from 'retro-react';
+import { Scrollbar } from 'retro-react';
 
 const App = () => {
   return (
@@ -14,6 +15,18 @@ const App = () => {
         particleColor="rainbow"
         particleSize={5}
       />
+      <Scrollbar theme="primary">
+  <Container
+    sx={{
+      backgroundColor: 'white',
+      height: '300px',
+      overflowY: 'scroll',
+      padding: '1rem',
+      width: '300px'
+    }}
+  >
+  </Container>
+</Scrollbar>
       <header className="App-header">
         <h1>Nostalgic Bedrooms</h1>
       </header>
