22615cbef9fe99f996cf9bbc680e51f4b8a2ee36
marktipton <marktipton95@gmail.com>
2024-08-07 13:21:44 -0500

    cards now appearing correctly

--- nostalgic-bedrooms-frontend/src/components/BedroomCard.js
+++ nostalgic-bedrooms-frontend/src/components/BedroomCard.js
@@ -1,13 +1,13 @@
-import React from 'react';
+// import React from 'react';
 
-const BedroomCard = ({ bedroom }) => {
-  return (
-    <div className="bedroom-card">
-      <h2>{bedroom.title}</h2>
-      <img src={bedroom.img_src} alt={bedroom.title} />
-      <p>{bedroom.description}</p>
-    </div>
-  );
-};
+// const BedroomCard = ({ bedroom }) => {
+//   return (
+//     <div className="bedroom-card">
+//       <h2>{bedroom.title}</h2>
+//       <img src={bedroom.img_src} alt={bedroom.title} />
+//       <p>{bedroom.description}</p>
+//     </div>
+//   );
+// };
 
-export default BedroomCard;
+// export default BedroomCard;
