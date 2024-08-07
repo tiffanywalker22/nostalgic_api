70fd31874f72aca0046852bc970b98a69b531e7f
marktipton <marktipton95@gmail.com>
2024-08-07 12:35:39 -0500

    images showing correctly

--- nostalgic-bedrooms-frontend/src/components/BedroomList.js
+++ nostalgic-bedrooms-frontend/src/components/BedroomList.js
@@ -16,7 +16,7 @@ const BedroomList = () => {
                 {bedrooms.map(bedroom => (
                     <div className="col-md-4" key={bedroom.id}>
                         <div className="card">
-                            <img src={`http://localhost:5000/${bedroom.image_url}`} className="card-img-top" alt={bedroom.name} />
+                            <img src={`http://localhost:5000/static/${bedroom.img_src}`} className="card-img-top" alt={bedroom.name} />
                             <div className="card-body">
                                 <h5 className="card-title">{bedroom.name}</h5>
                                 <p className="card-text">{bedroom.description}</p>
